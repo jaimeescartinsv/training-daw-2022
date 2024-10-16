7be232b3d5f0dbd50408f4a0c9c4a21cf9c5ed38
jaimeescartinsv <a27421@svalero.com>
2024-10-16 16:30:13 +0200

    ud3 ej1

--- UD3/01_scopes/main.js
+++ UD3/01_scopes/main.js
@@ -3,17 +3,17 @@
 var x = 10
 var test = 5
 
-console.log('valor de x ' + x)
+console.log('valor de x: ' + x)
 
 function shadow() {
     var x = 20
 
-    console.log('valor de x ' + x)
-    console.log('valor de test ' + test)
+    console.log('valor de x: ' + x)
+    console.log('valor de test: ' + test)
 }
 
 shadow()
-console.log('valor de x ' + x)
+console.log('valor de x: ' + x)
 
 
 function scopes() {
