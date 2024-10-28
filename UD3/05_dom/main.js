cc120cd90029a934f64563f7ae3b6973e5a6bc89
jaimeescartinsv <a27421@svalero.com>
2024-10-28 16:21:39 +0100

    ej 5 ud3

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,15 @@
+// Listar todos los divs
+const allDivs = document.querySelectorAll('div');
+console.log(allDivs);
+
+// Listar todos los elementos con la clase 'buttons'
+const buttonElements = document.querySelectorAll('.buttons');
+console.log(buttonElements);
+
+// Listar 1 div con clase 'buttons'
+const oneButtonDiv = document.querySelector('.buttons');
+console.log(oneButtonDiv);
+
+// Listar TODOS los divs con clase 'buttons'
+const allButtonDivs = document.querySelectorAll('.buttons');
+console.log(allButtonDivs);
