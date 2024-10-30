4437061c93d8ec0716b617a0944acd4ee8b7e06a
jaimeescartinsv <a27421@svalero.com>
2024-10-30 19:38:53 +0100

    ej6 table ud 3

--- UD3/06_modify_dom/table/main.js
+++ UD3/06_modify_dom/table/main.js
@@ -1,19 +1,35 @@
+let currentId = 1; // Variable para autoincrementar el ID
+
 function onClick() {
-    let tbody = document.getElementsByTagName('tbody')[0]
+    let tbody = document.getElementsByTagName('tbody')[0];
 
-    let tr = document.createElement('tr')
-    let td = document.createElement('td')
-    td.innerText = '0'
-    tr.appendChild(td)
+    let tr = document.createElement('tr');
 
-    td = document.createElement('td')
-    td.innerText = 'Example'
-    tr.appendChild(td)
+    // Crear la celda ID
+    let td = document.createElement('td');
+    td.innerText = currentId; 
+    tr.appendChild(td);
 
-    td = document.createElement('td')
-    td.innerText = 'Surname'
-    tr.appendChild(td)
+    // Crear la celda Name
+    td = document.createElement('td');
+    td.innerText = 'Example';
+    tr.appendChild(td);
 
-    tbody.appendChild(tr)
-    console.log('add')
+    // Crear la celda Surname
+    td = document.createElement('td');
+    td.innerText = 'Surname';
+    tr.appendChild(td);
+
+    // Crear la celda Acciones
+    td = document.createElement('td');
+    let editLink = document.createElement('a');
+    editLink.href = `http://localhost/edit/${currentId}`; 
+    editLink.innerText = 'Edit';
+    td.appendChild(editLink);
+    tr.appendChild(td);
+
+    tbody.appendChild(tr);
+    console.log('add');
+
+    currentId++; // Incrementar el ID para el siguiente elemento
 }
